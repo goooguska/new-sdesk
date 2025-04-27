e346746428a02a9a89bf76098132853a9e6c1792
a.buhtoyarov <a.buhtoyarov@qsoft.ru>
2025-04-27 16:07:40 +0300

    миграции

---
+++ backend/database/migrations/2025_04_27_122921_create_structure.php
@@ -0,0 +1,66 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('statuses', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->string('name');
+            $table->timestamps();
+        });
+
+        Schema::create('users', function (Blueprint $table) {
+            $table->id();
+            $table->string('full_name');
+            $table->string('email')->unique();
+            $table->string('login')->unique();
+            $table->string('password');
+            $table->foreignId('role_id')->constrained();
+            $table->timestamp('email_verified_at')->nullable();
+            $table->rememberToken();
+            $table->timestamps();
+        });
+
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->text('description');
+
+            $table->unsignedBigInteger('assignee_id');
+            $table->foreign('assignee_id')->references('id')->on('users');
+
+            $table->unsignedBigInteger('creator_id');
+            $table->foreign('creator_id')->references('id')->on('users');
+
+            $table->foreignId('status_id')->constrained();
+            $table->foreignId('category_id')->constrained();
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('roles');
+        Schema::dropIfExists('statuses');
+        Schema::dropIfExists('categories');
+        Schema::dropIfExists('users');
+        Schema::dropIfExists('tickets');
+    }
+};
